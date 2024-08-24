5d5868fe64b2027046f17b11a16c001d674616ae
Aaron <vodelsup@gmail.com>
2024-08-24 21:49:46 +0200

    remaps

--- lua/arken/plugins/telescope.lua
+++ lua/arken/plugins/telescope.lua
@@ -12,9 +12,7 @@ return {
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>sg', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
+        vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>sn', function()
             builtin.find_files { cwd = vim.fn.stdpath 'config' }
         end)
--- lua/arken/remap.lua
+++ lua/arken/remap.lua
@@ -1,13 +1,21 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
 
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+-- move code around (cool)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 -- center screen after operations
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("i", "<C-c>", "<Esc>")
+-- yank to system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({"n", "v"}, "<leader>Y", [["+Y]])
 
 -- prime convinced me
 vim.keymap.set(
