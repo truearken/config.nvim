cb734faca6968ae988c90032ddc4e433ca724758
Aaron <vodelsup@gmail.com>
2024-08-25 15:35:25 +0200

    changes

--- lua/arken/init.lua
+++ lua/arken/init.lua
@@ -1,3 +1,52 @@
 require("arken.set")
 require("arken.remap")
 require("arken.lazy")
+
+local autogrp = vim.api.nvim_create_augroup
+local arken_group = autogrp('arken', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = autogrp('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    group = arken_group,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+autocmd('LspAttach', {
+    group = arken_group,
+    callback = function(event)
+        local map = function(keys, func, mode)
+            mode = mode or 'n'
+            vim.keymap.set(mode, keys, func, { buffer = event.buf })
+        end
+
+        map('gd', require('telescope.builtin').lsp_definitions)
+        map('gr', require('telescope.builtin').lsp_references)
+        map('<leader>ds', require('telescope.builtin').lsp_document_symbols)
+        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols)
+        map('<leader>rn', vim.lsp.buf.rename)
+        map('<leader>ca', vim.lsp.buf.code_action, { 'n', 'x' })
+        map('ge', vim.diagnostic.goto_next)
+    end
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
