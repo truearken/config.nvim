5e2c26b7226676bdeadc1fd650c6b37115c29427
Aaron <Aaron.Koellner@afi-solutions.com>
2024-09-10 13:34:42 +0200

    visuals

--- lua/arken/plugins/style.lua
+++ lua/arken/plugins/style.lua
@@ -1,9 +1,18 @@
 return {
-    "folke/tokyonight.nvim",
+    "rose-pine/neovim",
+    as = "rose-pine",
     lazy = false,
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme 'tokyonight-night'
+        vim.cmd.colorscheme("rose-pine")
+
+        local groups = {
+            "Normal",
+            "NormalFloat"
+        }
+        for i = 1, #groups do
+            vim.api.nvim_set_hl(0, groups[i], { bg = "none" })
+        end
     end,
     opts = {},
 }
