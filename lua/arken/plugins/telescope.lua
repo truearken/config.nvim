83c3c2ded7a38cdc4c59be3c89f91d31f102a53d
Aaron <Aaron.Koellner@afi-solutions.com>
2024-09-12 14:00:47 +0200

    buffer search and smart search

--- lua/arken/plugins/telescope.lua
+++ lua/arken/plugins/telescope.lua
@@ -13,6 +13,7 @@ return {
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
         vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>sb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>sn', function()
             builtin.find_files { cwd = vim.fn.stdpath 'config' }
         end)
