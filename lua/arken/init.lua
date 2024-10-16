b021447c84a2cd0747582bc0a501c25e5cb0ef82
Aaron <Aaron.Koellner@afi-solutions.com>
2024-10-16 11:17:15 +0200

    commiting to eurkey layout

--- lua/arken/init.lua
+++ lua/arken/init.lua
@@ -9,32 +9,32 @@ local autocmd = vim.api.nvim_create_autocmd
 local yank_group = autogrp("HighlightYank", {})
 
 autocmd("TextYankPost", {
-    group = yank_group,
-    pattern = "*",
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 autocmd("LspAttach", {
-    group = arken_group,
-    callback = function(event)
-        local map = function(keys, func, mode)
-            mode = mode or "n"
-            vim.keymap.set(mode, keys, func, { buffer = event.buf })
-        end
+	group = arken_group,
+	callback = function(event)
+		local map = function(keys, func, mode)
+			mode = mode or "n"
+			vim.keymap.set(mode, keys, func, { buffer = event.buf })
+		end
 
-        map("gd", require("telescope.builtin").lsp_definitions)
-        map("gr", require("telescope.builtin").lsp_references)
-        map("gI", require("telescope.builtin").lsp_implementations)
-        map("<leader>ds", require("telescope.builtin").lsp_document_symbols)
-        map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols)
-        map("<leader>rn", vim.lsp.buf.rename)
-        map("<leader>ca", vim.lsp.buf.code_action, { "n", "x" })
-        map("<C-k>", vim.lsp.buf.signature_help, { "i" })
-        map("#", vim.diagnostic.goto_next)
-        map([[']], vim.diagnostic.goto_prev)
-    end
+		map("gd", require("telescope.builtin").lsp_definitions)
+		map("gr", require("telescope.builtin").lsp_references)
+		map("gI", require("telescope.builtin").lsp_implementations)
+		map("<leader>ds", require("telescope.builtin").lsp_document_symbols)
+		map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols)
+		map("<leader>rn", vim.lsp.buf.rename)
+		map("<leader>ca", vim.lsp.buf.code_action, { "n", "x" })
+		map("<C-k>", vim.lsp.buf.signature_help, { "i" })
+		map("[d", vim.diagnostic.goto_next)
+		map("]d", vim.diagnostic.goto_prev)
+	end,
 })
 
 vim.g.netrw_browse_split = 0
