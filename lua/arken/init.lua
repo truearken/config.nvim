82ec875da21189adf8c30b0e0b29e1a1f48173c5
Aaron <aaron.koellner@afi-solutions.com>
2025-01-28 14:47:57 +0100

    syntax

--- lua/arken/init.lua
+++ lua/arken/init.lua
@@ -24,11 +24,12 @@ autocmd("LspAttach", {
 			vim.keymap.set(mode, keys, func, { buffer = event.buf })
 		end
 
-		map("gd", require("telescope.builtin").lsp_definitions)
-		map("gr", require("telescope.builtin").lsp_references)
-		map("gI", require("telescope.builtin").lsp_implementations)
-		map("<leader>ds", require("telescope.builtin").lsp_document_symbols)
-		map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols)
+		local builtin = require("telescope.builtin")
+		map("gd", builtin.lsp_definitions)
+		map("gr", builtin.lsp_references)
+		map("gI", builtin.lsp_implementations)
+		map("<leader>ds", builtin.lsp_document_symbols)
+		map("<leader>ws", builtin.lsp_dynamic_workspace_symbols)
 		map("<leader>rn", vim.lsp.buf.rename)
 		map("<leader>ca", vim.lsp.buf.code_action, { "n", "x" })
 		map("<C-k>", vim.lsp.buf.signature_help, { "i" })
--- lua/arken/plugins/telescope.lua
+++ lua/arken/plugins/telescope.lua
@@ -1,22 +1,18 @@
 return {
-    "nvim-telescope/telescope.nvim",
+	"nvim-telescope/telescope.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		require("telescope").setup({})
 
-    tag = "0.1.5",
-
-    dependencies = {
-        "nvim-lua/plenary.nvim"
-    },
-
-    config = function()
-        require("telescope").setup({})
-
-        local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>sg", builtin.live_grep, {})
-        vim.keymap.set("n", "<leader>sd", builtin.diagnostics)
-        vim.keymap.set("n", "<leader><leader>", builtin.buffers, {})
-        vim.keymap.set("n", "<leader>sn", function()
-            builtin.find_files { cwd = vim.fn.stdpath "config" }
-        end)
-    end
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>sg", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>sd", builtin.diagnostics)
+		vim.keymap.set("n", "<leader><leader>", builtin.buffers, {})
+		vim.keymap.set("n", "<leader>sn", function()
+			builtin.find_files({ cwd = vim.fn.stdpath("config") })
+		end)
+	end,
 }
