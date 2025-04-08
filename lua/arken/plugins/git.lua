51e64f9742103d9b473f2f449308e0ac98f9cc23
Aaron <aaron.koellner@afi-solutions.com>
2025-04-08 09:44:35 +0200

    fugitive merge binds

--- lua/arken/plugins/git.lua
+++ lua/arken/plugins/git.lua
@@ -1,3 +1,7 @@
 return {
-    "tpope/vim-fugitive",
+	"tpope/vim-fugitive",
+	config = function()
+		vim.keymap.set("n", "tl", "<cmd>diffget //2<CR>")
+		vim.keymap.set("n", "tr", "<cmd>diffget //3<CR>")
+	end,
 }
