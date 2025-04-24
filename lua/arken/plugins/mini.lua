204d02a8a3d30c1096c48743cbba3e31707a6432
Aaron <aaron.koellner@afi-solutions.com>
2025-04-24 12:49:07 +0200

    fugitive branch in mini

--- lua/arken/plugins/mini.lua
+++ lua/arken/plugins/mini.lua
@@ -2,7 +2,11 @@ return {
 	"echasnovski/mini.nvim",
 	config = function()
 		require("mini.icons").setup()
-		require("mini.statusline").setup()
 		require("mini.diff").setup()
+		local statusline = require("mini.statusline")
+		statusline.section_git = function()
+			return "%{FugitiveStatusline()}"
+		end
+		statusline.setup()
 	end,
 }
