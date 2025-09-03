2006c7d5e20acafb61fba6cbcad52f7ba9d5c811
Aaron <aaron.koellner@afi-solutions.com>
2025-09-03 09:23:06 +0200

    auto format not always good

--- lua/arken/plugins/lsp.lua
+++ lua/arken/plugins/lsp.lua
@@ -107,19 +107,6 @@ return {
 				javascript = { "prettier" },
 				typescript = { "prettier" },
 			},
-			format_on_save = function(bufnr)
-				local disable_filetypes = { c = true, cpp = true }
-				local lsp_format_opt
-				if disable_filetypes[vim.bo[bufnr].filetype] then
-					lsp_format_opt = "never"
-				else
-					lsp_format_opt = "fallback"
-				end
-				return {
-					timeout_ms = 500,
-					lsp_format = lsp_format_opt,
-				}
-			end,
 		})
 		vim.keymap.set("n", "<leader>f", function()
 			require("conform").format({ async = true, lsp_format = "fallback" })
